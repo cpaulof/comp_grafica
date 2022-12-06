c11b427a471a8d84e4ab09169c3d51d0f35c7fd3
Paulo Fernando <paulof@acad.ifma.edu.br>
2022-12-06 19:59:26 -0300

    resolucao

--- atividade_6/iluminacao.py
+++ atividade_6/iluminacao.py
@@ -8,7 +8,7 @@ from OpenGL.GLUT import *
 
 screen_width = 1000
 screen_height = 800
-background_color = (0.2, 0.2, 0.2, 1)
+background_color = (0.0, 0.0, 0.0, 1.0)
 drawing_color = (1, 0.5, 0.5, 0.1)
 
 # screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
@@ -19,27 +19,27 @@ angle = 45
 
 def init():
 
-    luz_ambiente = (0.2, 0.4, 0.3, 1.0)
-    luz_difusa = (0.7, 0.7, 0.7, 1.0)
+    luz_ambiente = (0.2,0.2,0.2,1.0)
+    luz_difusa = (0.7,0.7,0.7,1.0)
     luz_especular = (1.0, 1.0, 1.0, 1.0)
-    posicao_luz = (0.5, 50.0, 50.0, 1.0)
+    posicao_luz = (0.0, 50.0, 50.0, 1.0)
 
     especularidade = (1.0, 1.0, 1.0, 1.0)
-    especMaterial = 50
+    especMaterial = 60
 
     glClearColor(*background_color)
 
     glShadeModel(GL_SMOOTH)
 
-    glMaterialfv(GL_FRONT, GL_SPECULAR, *especularidade)
+    glMaterialfv(GL_FRONT, GL_SPECULAR, especularidade)
     glMateriali(GL_FRONT,GL_SHININESS, especMaterial)
 
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, *luz_ambiente)
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luz_ambiente)
 
-    glLightfv(GL_LIGHT0, GL_AMBIENT, *luz_ambiente)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, *luz_difusa)
-    glLightfv(GL_LIGHT0, GL_SPECULAR, *luz_especular)
-    glLightfv(GL_LIGHT0, GL_POSITION, *posicao_luz)
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luz_ambiente)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luz_difusa)
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luz_especular)
+    glLightfv(GL_LIGHT0, GL_POSITION, posicao_luz)
 
     glEnable(GL_COLOR_MATERIAL)
     glEnable(GL_LIGHTING)
@@ -56,14 +56,14 @@ def visual_params():
     gluPerspective(angle, (screen_width / screen_height), 0.4, 500.0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    gluLookAt(100,90,200, 0,0,0, 0,1,0)
+    gluLookAt(0,80,200, 0,0,0, 0,1,0)
 
 
 
 
 def display_callback():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glColor3f(0.7, 0.7, 1.0)
+    glColor3f(0.0, 0.0, 1.0)
     glutSolidTeapot(50.0)
     glutSwapBuffers()
 
